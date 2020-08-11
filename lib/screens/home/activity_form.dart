71cef4c39db123446456dddf1ec31c7fcbeb44a8
zychen <zychen5186@gmail.com>
2020-08-11 11:46:15 +0800

    add activity_form

---
+++ lib/screens/home/activity_form.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Activityform extends StatefulWidget {
+  @override
+  _ActivityformState createState() => _ActivityformState();
+}
+
+class _ActivityformState extends State<Activityform> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          ''
+        )
+        TextFormField(
+
+        ),
+      ],
+
+      
+    );
+  }
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:Space/screens/home/activity_form.dart';
 import 'package:Space/screens/home/map.dart';
 import 'package:Space/services/auth.dart';
 import 'package:Space/shared/loading.dart';
@@ -28,29 +29,48 @@ class _HomeState extends State<Home> {
     super.initState();
   }
 
+    void _showActivityPanel(){
+      showModalBottomSheet(context: context, builder: (context){
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 60.0),
+            child: Activityform(),
+          );
+        }
+      );
+    }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text("Home"),
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        actions: [
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text("logout"),
-            onPressed: () async {
-              dynamic result = await _auth.signOut();
-            },
-          ),
-        ],
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _showActivityPanel(),
+          backgroundColor: Colors.brown[500],
+          child: Icon(Icons.add),
+        ),
+        
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text("Home"),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: [
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text("logout"),
+              onPressed: () async {
+                dynamic result = await _auth.signOut();
+              },
+            ),
+          ],
+        ),
+        body: (_currentLocation != null)
+            ? FireMap(
+                initialLocation: _currentLocation,
+              )
+            : Loading(),
       ),
-      body: (_currentLocation != null)
-          ? FireMap(
-              initialLocation: _currentLocation,
-            )
-          : Loading(),
     );
   }
 }
